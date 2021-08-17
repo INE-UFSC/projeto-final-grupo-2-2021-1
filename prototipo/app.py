d9ba580c2060388a1823d5b3f2761db241ca44fd
Luiz Felipe Cabral Lima <85948090+luizcabra@users.noreply.github.com>
2021-08-17 16:15:45 -0300

    mudanças para as entradas do cano

--- prototipo/app.py
+++ prototipo/app.py
@@ -1,8 +1,9 @@
 # Vamos tentar reunir nesse aqreuivo as funcionalidades do programa
 # De inicio é importante a gente apenas fazer o jogo funcionar e depois ir separando as coisas
 
-import pygame
+import pygame, sys
 from personagem import Personagem
+from cano import Cano
 from cenario import Cenario
 from pygame.locals import *
 
@@ -13,7 +14,11 @@ cenario = Cenario()
 clock = pygame.time.Clock()
 fps = 60
 
-passaro = Personagem(x=350, y=300)
+passaro = Personagem(x=150, y=300)
+
+canos = Cano(pygame.image.load('projeto-final-grupo-2-2021-1-main/prototipo/teste.png'))
+spawncano = pygame.USEREVENT
+pygame.time.set_timer(spawncano, 2000)
 
 rodando = True
 while rodando:
@@ -29,7 +34,9 @@ while rodando:
         if evento.type == pygame.KEYDOWN and not passaro.voando and not passaro.game_over:
             if evento.key == pygame.K_UP:
                 passaro.voando = True
-
+        if evento.type == spawncano:
+            canos.lista_canos.append(canos.criar_cano())
+            print(canos.lista_canos)
     pygame.display.update()
 
 pygame.quit()
