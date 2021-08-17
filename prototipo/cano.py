18aab4a1655fb303e2441610bcdec56477b2d848
Luiz Felipe Cabral Lima <85948090+luizcabra@users.noreply.github.com>
2021-08-17 16:15:05 -0300

    criação dos canos feitas
    ainda precisamos de bolar uma forma de criar dois canos com um espaço entre eles fixos, e fazer eles aparecerem na tela

--- prototipo/cano.py
+++ prototipo/cano.py
@@ -1,2 +1,15 @@
 # Aqui vamos construir os canos que sarão os obstáculos do nosso personagem
 # Temos que pensar como fazer os sistema de tamanho e posição aleatória
+import pygame
+
+
+class Cano:
+    def __init__(self, x):
+        self.cano = pygame.transform.scale2x(x)
+        self.lista_canos = []
+
+    def criar_cano(self):
+        cano = pygame.image.load('projeto-final-grupo-2-2021-1-main/prototipo/teste.png')
+        cano = pygame.transform.scale2x(cano)
+        novo = cano.get_rect(midtop = (382,308))
+        return novo
