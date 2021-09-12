6d7e2497b286f0f9f48ded8a9adf0d6f1198851a
Luiz Felipe Cabral Lima <85948090+luizcabra@users.noreply.github.com>
2021-09-12 11:47:52 -0300

    retirada função tela

--- prototipo/cenario.py
+++ prototipo/cenario.py
@@ -8,9 +8,6 @@ class Cenario:
         self.tela = pygame.display.set_mode((self.largura_tela, self.altura_tela))
         pygame.display.set_caption("Flappy Bird Ultimate Edition")
         self.velocidade_jogo = 0.5
-
-    def tela(self):
-        return self.tela
     
     def inicializa_tela(self):
         self.tela.fill((0, 0, 150))
