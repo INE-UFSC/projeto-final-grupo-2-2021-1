fa3eb3112986130667632a9102969fe6cc2d7208
Luiz Felipe Cabral Lima <85948090+luizcabra@users.noreply.github.com>
2021-09-14 10:49:41 -0300

    inserido menus e botões de volta
    os menus "highscore" e "como jogar" foram inserido, assim como as funções destes para voltar ao menu principal, ainda é necessario da função de pontuação para implementar as funcionalidades do highscore

--- prototipo/menu.py
+++ prototipo/menu.py
@@ -15,33 +15,34 @@ class Menu:
         texto_rect = superficie.get_rect()
         texto_rect.center = (x,y)
         self.jogo.cenario.tela.blit(superficie, texto_rect)
+        self.mainclock = pygame.time.Clock()
 
-    def menu(self):
-
+    def menu(self):          
         pygame.init()
         while True:
-            
-            self.jogo.cenario.inicializa_tela
+
+            self.jogo.cenario.inicializa_tela()
             self.desenha_texto('main menu', 20, 60, 20)
-            mainclock = pygame.time.Clock()
 
             mx, my = pygame.mouse.get_pos()
-            botao1 = pygame.Rect(50,100,300,75)
-            botao2 = pygame.Rect(50,250,300,75)
-            botao3 = pygame.Rect(50,400,300,75)
+            botao1 = pygame.Rect(240,250,300,75)
+            botao2 = pygame.Rect(240,350,300,75)
+            botao3 = pygame.Rect(240,450,300,75)
             pygame.draw.rect(self.jogo.cenario.tela, (255,0,0), botao1)
             pygame.draw.rect(self.jogo.cenario.tela, (0,255,0), botao2)
-            pygame.draw.rect(self.jogo.cenario.tela, (0,0,255), botao3)
+            pygame.draw.rect(self.jogo.cenario.tela, (255,0,255), botao3)
 
             if botao1.collidepoint((mx,my)):
                 if self.click:
                     self.jogo.iniciar()
             if botao2.collidepoint((mx,my)):
                 if self.click:
-                    pass
+                    self.highscore()
             if botao3.collidepoint((mx,my)):
                 if self.click:
-                    pass
+                    self.como_jogar()
+
+            self.click = False
 
             for event in pygame.event.get():
                 if event.type == QUIT:
@@ -52,10 +53,65 @@ class Menu:
                     if event.key == K_ESCAPE:
                         pygame.quit()
                         sys.exit
-                    
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:
                         self.click = True
 
             pygame.display.update()
-            mainclock.tick(const.fps)
+            self.mainclock.tick(const.fps)
+
+    def highscore(self):
+
+        while True:
+            self.jogo.cenario.inicializa_tela()
+            self.desenha_texto('Highscore', 20, 60, 20)
+
+            botao4 = pygame.Rect(20,575,100,50)
+            pygame.draw.rect(self.jogo.cenario.tela, (255,255,0), botao4)
+            
+            mx, my = pygame.mouse.get_pos()
+
+            if botao4.collidepoint((mx,my)):
+                if self.click:
+                    break
+            
+            self.click = False
+
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        self.click = True
+
+            pygame.display.update()
+            self.mainclock.tick(const.fps)
+
+    def como_jogar(self):
+
+        while True:
+            self.jogo.cenario.inicializa_tela()
+            self.desenha_texto('Como jogar', 20, 60, 20)
+
+            botao5 = pygame.Rect(20,575,100,50)
+            pygame.draw.rect(self.jogo.cenario.tela, (255,0,0), botao5)
+            
+            mx, my = pygame.mouse.get_pos()
+
+            if botao5.collidepoint((mx,my)):
+                if self.click:
+                    break
+            
+            self.click = False
+
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        self.click = True
+
+            pygame.display.update()
+            self.mainclock.tick(const.fps)
