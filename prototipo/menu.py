a75eb71c3d0e89a5f58ec7e88c5b3c0492dc694f
Luiz Felipe Cabral Lima <85948090+luizcabra@users.noreply.github.com>
2021-09-17 15:23:45 -0300

    reduzindo a proceduralidade de menu.py

--- prototipo/menu.py
+++ prototipo/menu.py
@@ -39,10 +39,15 @@ class Menu:
     def resetaclick(self):
         self.click = False
     
-    def desenhabotao(self, x, y, tamanhox, tamanhoy, cor):
+    def botao(self, x, y, tamanhox, tamanhoy):
+        acao = False
         botao = pygame.Rect(x, y, tamanhox, tamanhoy)
-        pygame.draw.rect(self.jogo.cenario.tela, cor, botao)
-        return botao
+        pygame.draw.rect(self.jogo.cenario.tela, (255,0,0), botao)
+        
+        if botao.collidepoint(pygame.mouse.get_pos()):
+            if self.click:
+                acao = True
+        return acao
 
     def menu(self):          
         pygame.init()
@@ -51,19 +56,12 @@ class Menu:
             self.jogo.cenario.inicializa_tela()
             self.desenha_texto('main menu', 20, 60, 20)
 
-            botao1 = self.desenhabotao(240,250,300,75, (255,0,0))
-            botao2 = self.desenhabotao(240,350,300,75, (0,255,0))
-            botao3 = self.desenhabotao(240,450,300,75, (255,255,0))
-
-            if botao1.collidepoint((pygame.mouse.get_pos())):
-                if self.click:
-                    self.jogo.iniciar()
-            if botao2.collidepoint((pygame.mouse.get_pos())):
-                if self.click:
-                    self.highscore()
-            if botao3.collidepoint((pygame.mouse.get_pos())):
-                if self.click:
-                    self.como_jogar()
+            if self.botao(240,250,300,75):
+                self.jogo.iniciar()
+            if self.botao(240,350,300,75):
+                self.highscore()
+            if self.botao(240,450,300,75):
+                self.como_jogar()
 
             self.resetaclick()
             self.eventos_menu()
@@ -75,11 +73,8 @@ class Menu:
             self.jogo.cenario.inicializa_tela()
             self.desenha_texto('Highscore', 20, 60, 20)
 
-            botao4 = self.desenhabotao(20,575,100,50, (255,255,0))
-
-            if botao4.collidepoint((pygame.mouse.get_pos())):
-                if self.click:
-                    break
+            if self.botao(20,575,100,50):
+                break
             
             self.resetaclick()
             self.eventos_menu()
@@ -91,11 +86,8 @@ class Menu:
             self.jogo.cenario.inicializa_tela()
             self.desenha_texto('Como jogar', 20, 60, 20)
 
-            botao4 = self.desenhabotao(20,575,100,50, (255,255,0))
-
-            if botao4.collidepoint((pygame.mouse.get_pos())):
-                if self.click:
-                    break
+            if self.botao(20,575,100,50):
+                break
             
             self.resetaclick()
             self.eventos_menu()
