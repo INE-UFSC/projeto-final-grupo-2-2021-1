9b00d503c4cdee900257d16dfd38f3c0f8b06035
Luiz Felipe Cabral Lima <85948090+luizcabra@users.noreply.github.com>
2021-09-22 14:09:51 -0300

    Create menugameover.py

---
+++ versao_final/menugameover.py
@@ -0,0 +1,33 @@
+from menu import Menu
+from pygame.constants import K_ESCAPE, K_KP_ENTER, K_SPACE, KEYDOWN, MOUSEBUTTONDOWN, QUIT
+from controlador import Controlador
+from constantes import Constante
+from abc import ABC, abstractmethod
+import pygame, sys
+
+class MenuGameOver(Menu):
+    def __init__(self):
+        super().__init__()
+        self.estado = 'Game over'
+
+    def desenha_botao(self):
+        acao = False
+        if self.cria_botao(20,575,100,50).collidepoint(pygame.mouse.get_pos()):
+            if self.click:
+                acao = True
+        return acao
+
+    def menu(self):
+        
+        pygame.init()
+        while True:
+            self.cenario.inicializa_tela()
+            self.desenha_texto(self.estado, 20, 60, 20)
+
+            if self.desenha_botao():
+                self.estado = 'Main Menu'
+                break
+            
+            self.resetaclick()
+            self.eventos_menu()
+            self.atualizatela()
