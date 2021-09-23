9dcc1363bbca9ae82e3bfa24785414cf4f2002f9
Patrícia <patriciab.arigoni17@gmail.com>
2021-09-23 10:21:48 -0300

    Encapsulamento dos atributos das classes Item, ItemInvencibilidade e ItemTamanho, bem como retirada de atributos desnecessários

--- versao_final/item.py
+++ versao_final/item.py
@@ -1,60 +1,95 @@
-from abc import ABC, abstractmethod
-import pygame
 import random
-from personagem import Personagem
-from contador import Contador
 from constantes import Constante
+from abc import ABC, abstractmethod
 
-# Aqui vamos construir os canos que sarão os obstáculos do nosso personagem
-# Temos que pensar como fazer os sistema de tamanho e posição aleatória
 
 class Item(ABC):
     def __init__(self):
-        self.const = Constante()
-        self.itens = []  
-        self.criado = False
-        self.colidiu = False
-        self.ativo = False
-        self.final_efeito = 0
+        self.__const = Constante()
         self.__x = 0
         self.__y = 0
-        self.__timer = None
-        self.cor = (160, 160, 160)
+        self.__criado = False
+        self.__colidiu = False
+        self.__ativo = False
+        self.__final_efeito = 0
+    
+    @property
+    def const(self):
+        return self.__const
 
     @property
     def x(self):
         return self.__x
     
     @property
-    def timer(self):
-        return self.__timer
+    def y(self):
+        return self.__y
     
-    @timer.setter
-    def timer(self, tempo):
-        self.__timer = tempo
+    @property
+    def criado(self):
+        return self.__criado
+    
+    @property
+    def colidiu(self):
+        return self.__colidiu
+    
+    @property
+    def ativo(self):
+        return self.__ativo
+    
+    @property
+    def final_efeito(self):
+        return self.__final_efeito
+    
+    @const.setter
+    def const(self, const):
+        self.__const = const
+    
+    @x.setter
+    def x(self, x):
+        self.__x = x
+    
+    @y.setter
+    def y(self, y):
+        self.__y = y
+    
+    @criado.setter
+    def criado(self, criado):
+        self.__criado = criado
+    
+    @colidiu.setter
+    def colidiu(self, colidiu):
+        self.__colidiu = colidiu
+    
+    @ativo.setter
+    def ativo(self, ativo):
+        self.__ativo = ativo
+    
+    @final_efeito.setter
+    def final_efeito(self, final_efeito):
+        self.__final_efeito = final_efeito
 
     def atualiza(self, tela_jogo): #Faz todo o processo de gerar o item, aplicar o movimento e destruir
-        if not self.criado:
+        if not self.__criado:
             self.define_posicao_tela()
         self.desenha_objeto(tela_jogo)
 
     def define_posicao_tela(self): #Sorteia uma posicão que o item vai ficar na tela
 
-        range_inicio_y = self.const.distancia_item_cano
-        range_final_y = self.const.tela_jogo_altura - self.const.distancia_item_cano
+        range_inicio_y = self.__const.distancia_item_cano
+        range_final_y = self.__const.tela_jogo_altura - self.__const.distancia_item_cano
 
         self.__y = random.randint(range_inicio_y, range_final_y)
 
-        range_inicio_x = self.const.tela_jogo_largura + self.const.distancia_item_cano + self.const.largura_cano
-        range_final_x = self.const.tela_jogo_largura + \
-                    (self.const.tela_jogo_largura - (self.const.posicao_gera_cano + self.const.distancia_item_cano))
+        range_inicio_x = self.__const.tela_jogo_largura + self.__const.distancia_item_cano + self.__const.largura_cano
+        range_final_x = self.__const.tela_jogo_largura + \
+                    (self.__const.tela_jogo_largura - (self.__const.posicao_gera_cano + self.__const.distancia_item_cano))
 
         self.__x = random.randint(range_inicio_x, range_final_x)
-        self.criado = True
+        self.__criado = True
 
     def gera_retangulo(self):    # gera o retângulo que representa a posição do item
         rect = self.animacao_item.sprites()[0].rect
-
         return [rect]
 
     def desenha_objeto(self, tela_jogo): #Desenha o item na tela do jogo
@@ -68,15 +103,14 @@ class Item(ABC):
         self.efeito(personagem)
 
     def tempo(self, tempo_atual):
-        self.final_efeito = tempo_atual + self.tempo_item()
+        self.__final_efeito = tempo_atual + self.tempo_item()
 
     def duracao_item(self, contador):
-        
-        if self.colidiu == True:
+        if self.__colidiu:
             self.tempo(int(contador.tempo_contado))
-            self.colidiu = False
+            self.__colidiu = False
 
-        if self.final_efeito == contador.tempo_contado:
+        if self.__final_efeito == contador.tempo_contado:
             self.reverter()
 
     @abstractmethod
@@ -90,5 +124,3 @@ class Item(ABC):
     @abstractmethod
     def reverter(self):  # reverte o efeito do item
         pass
-
-
--- versao_final/itemInvencibilidade.py
+++ versao_final/itemInvencibilidade.py
@@ -1,31 +1,41 @@
 # Item com efeito de tornar o personagem imune a colisões com os canos
 # Ainda pode haver "game over", caso o personagem atinja o chão ou o topo da tela 
 
-from item import Item
-from constantes import Constante
-from animacao import ItemInvencivelAnimacao
 import pygame
+from item import Item
+from animacao import ItemInvencivelAnimacao
+
 
 class ItemInvencibilidade(Item):
     def __init__(self):
         super().__init__()
-        self.tempo_efeito = self.const.tempo_invencibilidade
         self.__animacao_item = pygame.sprite.Group(ItemInvencivelAnimacao())
+        self.__tempo_efeito = self.const.tempo_invencibilidade
 
     @property
     def animacao_item(self):
         return self.__animacao_item
+    
+    @property
+    def tempo_efeito(self):
+        return self.__tempo_efeito
+    
+    @animacao_item.setter
+    def animacao_item(self, animacao_item):
+        self.__animacao_item = animacao_item
+    
+    @tempo_efeito.setter
+    def tempo_efeito(self, tempo_efeito):
+        self.__tempo_efeito = tempo_efeito
 
     def efeito(self, personagem):
         self.__personagem = personagem
         self.__personagem.invencivel = True
-        self.__personagem.cor = (0, 0, 0)
         self.colidiu = True
         self.ativo = True
 
     def reverter(self):
         self.__personagem.invencivel = False
-        self.__personagem.cor = (255, 0, 0)
         self.ativo = False
 
     def tempo_item(self):
