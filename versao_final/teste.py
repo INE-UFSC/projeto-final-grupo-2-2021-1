8d46f19900477b192033770fe91c93f06a2d6fc6
Patrícia <patriciab.arigoni17@gmail.com>
2021-09-22 23:35:32 -0300

    Atualizado arquivo app.py

--- versao_final/teste.py
+++
@@ -1,8 +0,0 @@
-# Arquivo temporário para testar o funcionamento do controlador
-
-from controlador import Controlador
-from menucontroller import MenuController
-
-
-control = MenuController()
-control.main_menu()
